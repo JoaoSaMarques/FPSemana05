7d12510b505d6cd312176eb388bc3625144350ce
JoaoSaMarques <joao.sa.marques@hotmail.com>
2024-12-03 14:31:48 +0000

    remove white spaces

--- Calculadora.py
+++ Calculadora.py
@@ -13,18 +13,18 @@ def Divide(x, y):
 print("Select operation.\n 1. Multiply \n 2. Add\n 3. Subtract\n 4. Divide")
 
 while True:
-    choice = input("Enter between 1/2/3/4\n 'Exit' to quit.")
+    choice = input("Enter between 1/2/3/4")
     num1 = int(input("First number here: "))
     num2 = int(input("Second number here: "))
     
     if choice == '1':
-        print(num1, "* ", num2, "=", Multiply(num1, num2))
+        print(num1, "*", num2, "=", Multiply(num1, num2))
         
     elif choice == '2':
-        print(num1, "+ ", num2, "=", Add(num1, num2))
+        print(num1, "+", num2, "=", Add(num1, num2))
         
     elif choice == '3':
-        print(num1, "- ", num2, "=", Subtract(num1, num2))
+        print(num1, "-", num2, "=", Subtract(num1, num2))
         
     elif choice == '4':
         print(num1, "/", num2, "=", Divide(num1, num2))
