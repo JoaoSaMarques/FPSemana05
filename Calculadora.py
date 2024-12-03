1518e156545ad29ac1151dbca23db5b7f2b9bd20
JoaoSaMarques <joao.sa.marques@hotmail.com>
2024-12-03 14:25:49 +0000

    remove calculator and add choices print

--- Calculadora.py
+++ Calculadora.py
@@ -1,14 +1,15 @@
-def Calculator():
+   
+def Multiply(x, y):
+    return x * y
+        
+def Add(x, y):
+    return x + y
+        
+def Subtract(x, y):
+    return x - y
+        
+def Divide(x, y):
+    return x / y
     
-    def Multiply(x, y):
-        return x * y
-        
-    def Add(x, y):
-        return x + y
-        
-    def Subtract(x, y):
-        return x - y
-        
-    def Divide(x, y):
-        return x / y
-    
+print("Select operation.\n 1. Multiply \n 2. Add\n 3. Subtract\n 4. Divide")
+
